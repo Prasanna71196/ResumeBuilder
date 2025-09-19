82c6a4b8014238c67de3259df575d7b737b4b962
Prasanna71196 <shaktiprasanna7.11.1996@gmail.com>
2025-09-19 22:50:24 +0530

    Flatten project structure for Render deployment

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -7,14 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import sys
 import os
-
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
